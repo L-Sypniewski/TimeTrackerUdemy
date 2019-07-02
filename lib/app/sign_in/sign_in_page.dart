7830fb466fedffbef83229b116b24d7ff23aa7fd
Łukasz Sypniewski <l.sypniewski@gmail.com>
2019-07-02 22:52:12 +0200

    After lesson #112

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/sign_in_button.dart';
 import 'package:time_tracker_flutter_course/common_widgets/custom_raised_button.dart';
 
+import 'social_sign_in_button.dart';
+
 class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -28,15 +30,52 @@ class SignInPage extends StatelessWidget {
             style: TextStyle(fontSize: 32, fontWeight: FontWeight.w600),
           ),
           SizedBox(
-            height: 8,
+            height: 48,
+          ),
+          SocialSignInButton(
+            assetName: 'images/google-logo.png',
+            text: "Sign in with Google",
+            textColor: Colors.black87,
+            color: Colors.white,
+            onPressed: () {},
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          SocialSignInButton(
+            assetName: 'images/facebook-logo.png',
+            text: "Sign in with Facebook",
+            textColor: Colors.white,
+            color: Color(0xFF334D92),
+            onPressed: () {},
           ),
           SizedBox(
             height: 8.0,
           ),
           SignInButton(
-            text: "Sign in with Google",
-            textColor: Colors.black87,
-            color: Colors.white,
+            text: "Sign in with email",
+            textColor: Colors.white,
+            color: Colors.teal[700],
+            onPressed: () {},
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          Text(
+            'or',
+            style: TextStyle(
+              fontSize: 14,
+              color: Colors.black87,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          SignInButton(
+            text: "Go anonymus",
+            textColor: Colors.black,
+            color: Colors.lime[300],
             onPressed: () {},
           ),
         ],
